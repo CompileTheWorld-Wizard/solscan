ccae899413cb987e9b8820fb481837b079d6a6c4
DavitK000000 <davitkhimshiashvili96@gmail.com>
2025-12-10 07:41:31 -0800

    Test extracting bonding curve status

--- src/services/utils/transactionUtils.ts
+++ src/services/utils/transactionUtils.ts
@@ -23,6 +23,7 @@ export interface MintBondingCurvePair {
  */
 export function extractAllMintBondingCurvePairs(tx: any): MintBondingCurvePair[] {
   try {
+    console.log(tx.transaction.message.compiledInstructions)
     // Check if compiledInstructions exists in various possible locations
     const compiledInstructions = 
       tx?.transaction?.message?.compiledInstructions ||
